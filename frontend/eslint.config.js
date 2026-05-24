fa3c5c07ec9b855fa16622416ee0c2a77d4ea6aa
Ángel González <sneyder.gonzalez156@gmail.com>
2026-05-23 22:47:08 -0500

    fix: disable eslint in pipeline

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,15 +2,27 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import babelParser from "@babel/eslint-parser";
 
 export default [
   js.configs.recommended,
+
   {
     files: ["**/*.{js,jsx}"],
 
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+
+      // 🔴 CLAVE: habilita JSX correctamente
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+
       globals: globals.browser,
     },
 
@@ -21,6 +33,9 @@ export default [
 
     rules: {
       ...reactHooks.configs.recommended.rules,
+
+      // opcional (recomendado para Vite/React)
+      "react-refresh/only-export-components": "warn",
     },
   },
 ];
